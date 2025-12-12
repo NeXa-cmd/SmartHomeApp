601b6f7f70d4b1124cb675f88d501f01da2bc7d6
NeXa-cmd <memopeo41@gmail.com>
2025-12-12 15:18:00 +0100

    feat: Started implementation of Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+// Header Component - Title bar for the app
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = ({ title }) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+// TODO: Complete remaining implementation (40% done)
